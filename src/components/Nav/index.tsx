ae530260f891a7f2cb1ef9b8ac2b1d2aacaaf9d5
Edwin Garcés <edwingarcessaucedo@gmail.com>
2022-09-28 22:02:24 -0500

    fix(nav): main logo routing fixed

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -7,7 +7,7 @@ const Nav = () => (
   <nav className="Nav row center-xs">
     <ul>
       <li>
-        <Link to="/">
+        <Link to="/trivia">
           <img className="Nav__logo" src={logo} alt="Logo" />
         </Link>
       </li>
